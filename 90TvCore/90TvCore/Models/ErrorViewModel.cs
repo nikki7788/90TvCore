2ab02f7cfb241186b580d66fa5e7b1c2ae03f1b8
NIKBAKHT\sarir <hn0297@gmail.com>
2019-02-26 21:52:42 +0330

    changes to correct search box and bower json for bootstrap 4.1.3 &jquery 3.3.1 adding girignore & gitattribute

---
+++ 90TvCore/90TvCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _90TvCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
